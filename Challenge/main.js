6881068c3eb095ee68a02e7b7d7a84afaa66e679
Philipp Dorner <pdorner@gmx.net>
2019-10-07 13:25:30 +0200

    Einkaufswagen first try

---
+++ Challenge/main.js
@@ -0,0 +1,8 @@
+arr = [];
+
+function addToCart(id, img, name, price){
+	pro = new Product (img, name, price);
+	arr.push(pro);
+
+
+}
---
+++ Challenge/product.js
@@ -0,0 +1,10 @@
+class Product{
+	constructor(id, img, name, price){
+		this.id = id;
+		this.img = img;
+		this.name = name;
+		this.price = price;
+	}
+}
+
+
