c5fa4d64dd767545a14ac707ebcc0e7fb853dd76
drewitzmax <drewitz.max@googlemail.com>
2019-10-07 15:12:01 +0200

    creating elements from localstorage

--- Challenge/main.js
+++ Challenge/main.js
@@ -15,3 +15,20 @@ function changeHTML() {
   localStorage.myArray = JSON.stringify(arr);
 
 }
+
+function printCart(arr){
+	for(let i=0; i<arr.length;i++){
+		let itemCard = document.createElement("div");
+		itemCard.class = "itemCard"
+		let image = document.createElement("img");
+		image.src= arr[i].img;
+		let name = document.createElement("h1");
+		name.textContent = arr[i].name;
+		let price = document.createElement("p");
+		price.textContent = arr[i].price;
+		itemCard.appendChild(name);
+		itemCard.appendChild(image);
+		itemCard.appendChild(price);
+		document.getElementById("einkauf").appendChild(itemCard);
+	}
+}
