6b324f751aa693efd1967d8bdb8c2fc083763f65
drewitzmax <drewitz.max@googlemail.com>
2019-10-07 14:09:31 +0200

    fixing onclick argument

--- Challenge/main.js
+++ Challenge/main.js
@@ -1,10 +1,9 @@
 arr = [];
 
 function addToCart(id){
-	let card = document.getElementsByClassName("productCard")[id];
-	let name = card.getElementsByTagName("h1").textContent;
-	let img = card.getElementsByTagName("img").src;
-	let price = card.getElementsByClassName("price").value;
+	let name = card.getElementsByClassName("name")[id].textContent;
+	let img = card.getElementsByClassName("imgProduct")[id].src;
+	let price = card.getElementsByClassName("price")[id].value;
 	pro = new Product (img, name, price);
 	arr.push(pro);
 }
